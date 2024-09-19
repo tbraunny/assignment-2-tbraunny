4938c79b5371bdee631138022753dadf422e9645
Thomas <tbraun@nevada.unr.edu>
2024-09-18 18:44:27 -0700

    remove matplotlib

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -2,23 +2,45 @@ import numpy as np
 import argparse
 import scipy.io
 from sklearn.cluster import KMeans
+#import matplotlib.pyplot as plt
 
 class MykmeansClustering:
     def __init__(self, dataset_file):
         self.model = None
+        self.mat = None
         
         self.dataset_file = dataset_file
         self.read_mat()
 
     def read_mat(self):
         mat = scipy.io.loadmat(self.dataset_file)
+        #print(mat.keys())
+
+        self.data = mat['X']
+        #print(self.data)
         
     def model_fit(self):
-        '''
-        initialize self.model here and execute kmeans clustering here
-        '''
+        self.model = KMeans(3)
+        self.model.fit(self.data)
         
         cluster_centers = np.array([[0,0]])
+
+        cluster_centers = self.model.cluster_centers_
+        cluster_labels = self.model.labels_
+
+        # plot cluster centers
+        '''
+        x = cluster_centers[:,0]
+        y = cluster_centers[:,1]
+        x_data = self.data[:,0]
+        y_data = self.data[:,1]
+        
+        plt.scatter(x_data , y_data , c=cluster_labels)
+        plt.scatter(x , y , color='red' , marker='X')
+        plt.title('Cluster Centers')
+        plt.show()
+        '''
+
         return cluster_centers
 
 if __name__ == '__main__':
@@ -27,5 +49,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     classifier = MykmeansClustering(args.dataset_file)
     clusters_centers = classifier.model_fit()
-    print(clusters_centers)
     
--- logistic_regression.py
+++ logistic_regression.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression,LinearRegression
 import argparse
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+#import matplotlib.pyplot as plt
 
 class MyLogisticRegression:
     def __init__(self, dataset_num, perform_test):
@@ -30,55 +31,101 @@ class MyLogisticRegression:
             print("unsupported dataset number")
             
         self.training_set = pd.read_csv(train_dataset_file, sep=',', header=0)
+
         if self.perform_test:
             self.test_set = pd.read_csv(test_dataset_file, sep=',', header=0)
-        
+            self.X_test = self.test_set[['exam_score_1' , 'exam_score_2']]
+            self.y_test = self.test_set[['label']]        
+
+        # format train data by column
+        self.X_train = self.training_set[['exam_score_1' , 'exam_score_2']]
+        self.y_train = self.training_set['label']
         
     def model_fit_linear(self):
-        '''
-        initialize self.model_linear here and call the fit function
-        '''
-        pass
+        self.model_linear = LinearRegression(fit_intercept=True)
+        self.model_linear.fit(self.X_train,self.y_train)
+    
+    def train_linear_model(self):
+        self.read_csv(self.dataset_num)
+        self.model_fit_linear()
+
+    def train_logistic_model(self):
+        self.read_csv(self.dataset_num)
+        self.model_fit_logistic()
     
     def model_fit_logistic(self):
-        '''
-        initialize self.model_logistic here and call the fit function
-        '''
-
-        pass
+        self.model_logistic = LogisticRegression()
+        self.model_logistic.fit(self.X_train , self.y_train)
     
     def model_predict_linear(self):
-        '''
-        Calculate and return the accuracy, precision, recall, f1, support of the model.
-        '''
-        self.model_fit_linear()
+        self.train_linear_model()
         accuracy = 0.0
         precision, recall, f1, support = np.array([0,0]), np.array([0,0]), np.array([0,0]), np.array([0,0])
         assert self.model_linear is not None, "Initialize the model, i.e. instantiate the variable self.model_linear in model_fit_linear method"
-        assert self.training_set is not None, "self.read_csv function isn't called or the self.trianing_set hasn't been initialized "
+
+        if self.X_train is not None:
+
+            y_pred = self.model_linear.predict(self.X_test)
+            y_pred_binary = (y_pred > 0.5).astype(bool)
+
+            accuracy = self.model_linear.score(self.X_test , self.y_test.values.reshape(-1 , 1))
+            precision , recall , f1 , support = precision_recall_fscore_support(self.y_test.values.reshape(-1 , 1) , y_pred_binary)   
         
-        if self.X_test is not None:
-            # perform prediction here
-            pass
+        assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"        
+        return [accuracy, precision, recall, f1, support]
+
+    def model_predict_logistic(self):
+        self.train_logistic_model()
+        accuracy = 0.0
+        precision, recall, f1, support = np.array([0,0]), np.array([0,0]), np.array([0,0]), np.array([0,0])
+
+        assert self.model_logistic is not None, "Initialize the model, i.e. instantiate the variable self.model_logistic in model_fit_logistic method"
+        assert self.training_set is not None, "self.read_csv function isn't called or the self.trianing_set hasn't been initialized "
+
+        if self.X_train is not None:
+            y_pred = self.model_logistic.predict(self.X_test)
+            accuracy = accuracy_score(self.y_test.values.reshape(-1 , 1) , y_pred)
+            
+            precision , recall , f1 , support = precision_recall_fscore_support(self.y_test.values.reshape(-1 , 1) , y_pred)
+            
         
         assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
         return [accuracy, precision, recall, f1, support]
 
-    def model_predict_logistic(self):
-        '''
-        Calculate and return the accuracy, precision, recall, f1, support of the model.
-        '''
-        self.model_fit_logistic()
-        accuracy = 0.0
-        precision, recall, f1, support = np.array([0,0]), np.array([0,0]), np.array([0,0]), np.array([0,0])
-        assert self.model_logistic is not None, "Initialize the model, i.e. instantiate the variable self.model_logistic in model_fit_logistic method"
-        assert self.training_set is not None, "self.read_csv function isn't called or the self.trianing_set hasn't been initialized "
-        if self.X_test is not None:
-            # perform prediction here
-            pass
-        
-        assert precision.shape == recall.shape == f1.shape == support.shape == (2,), "precision, recall, f1, support should be an array of shape (2,)"
-        return [accuracy, precision, recall, f1, support]
+
+    '''
+    def plot_graphs_linear(self):
+        # exam 1 against exam 2 scores
+        x1 = self.training_set['exam_score_1']
+        x2 = self.training_set['exam_score_2']
+        p1 , p2 = np.polyfit(x1 , x2 , 1)
+        plt.plot(x2 , p1 * x2 + p2 , color='blue')
+        plt.scatter(x1 , x2 , c=self.y_train)
+        plt.xlabel("Exam 1 Scores")
+        plt.ylabel("Exam 2 Scores")
+        plt.title("Exam 1 vs. Exam 2 Scores (Linear - Dataset 2)")
+        plt.show()
+
+    def plot_graphs_logistic(self):
+        # exam 1 against exam 2 scores
+        x1 = self.training_set['exam_score_1'].values
+        x2 = self.training_set['exam_score_2'].values
+
+        # decision boundary
+        x_min , x_max = x1.min() - 1 , x2.max() + 1
+        y_min , y_max = x2.min() - 1 , x2.max() + 1
+        xx , yy = np.meshgrid(np.arange(x_min , x_max , 0.01) ,
+                              np.arange(y_min , y_max , 0.01))
+        z = self.model_logistic.predict(pd.DataFrame(np.c_[xx.ravel() , yy.ravel()] , columns=['exam_score_1' , 'exam_score_2']))
+        z = z.reshape(xx.shape)
+        plt.contourf(xx , yy , z , alpha=0.8)       
+        plt.scatter(x1 , x2 , c=self.y_train , edgecolors='k' , marker='o' , s=100)
+
+        plt.xlabel("Exam 1 Scores")
+        plt.ylabel("Exam 2 Scores")
+        plt.title("Exam 1 vs. Exam 2 Scores (Logistic - Dataset 2)")
+        plt.show()
+    '''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Linear Regression')
@@ -88,4 +135,8 @@ if __name__ == '__main__':
     classifier = MyLogisticRegression(args.dataset_num, args.perform_test)
     acc = classifier.model_predict_linear()
     acc = classifier.model_predict_logistic()
+
+    # plots
+    #classifier.plot_graphs_linear()
+    #classifier.plot_graphs_logistic()
     
